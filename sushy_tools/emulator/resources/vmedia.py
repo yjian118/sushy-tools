441f8e5ec07749c75c625cf43456af9322c9dd0d
Yuxing Jiang <Yuxing.Jiang@windriver.com>
2024-10-23 17:09:44 -0400

    Backoff retry and extended timeout to insert image
    Signed-off-by: Yuxing Jiang <Yuxing.Jiang@windriver.com>

--- sushy_tools/emulator/resources/vmedia.py
+++ sushy_tools/emulator/resources/vmedia.py
@@ -12,10 +12,13 @@
 #    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 #    License for the specific language governing permissions and limitations
 #    under the License.
+# Copyright (c) 2024 Wind River Systems, Inc.
+#
 
 import os
 import re
 import tempfile
+import time
 from urllib import parse as urlparse
 
 import requests
@@ -127,8 +130,9 @@ class StaticDriver(base.DriverBase):
                 device_info.get('Inserted', False),
                 device_info.get('WriteProtected', False))
 
-    def insert_image(self, identity, device, image_url,
-                     inserted=True, write_protected=True):
+    def insert_image(
+        self, identity, device, image_url, inserted=True, write_protected=True
+    ):
         """Upload, remove or insert virtual media
 
         :param identity: parent resource ID
@@ -139,59 +143,102 @@ class StaticDriver(base.DriverBase):
         :param write_protected: prevent write access the inserted media
         :raises: `FishyError` if image can't be manipulated
         """
+        MAX_CREATE_LOCAL_IMAGE_RETRIES = 5
+        INITIAL_BACKOFF = 1  # in seconds
+        BACKOFF_MULTIPLIER = 2
+
         device_info = self._get_device(identity, device)
+        retries = 0
+        backoff = INITIAL_BACKOFF
 
-        try:
-            with tempfile.NamedTemporaryFile(
-                    mode='w+b', delete=False) as tmp_file:
-
-                with requests.get(image_url, stream=True, verify=False) as rsp:
-
-                    with open(tmp_file.name, 'wb') as fl:
-
-                        for chunk in rsp.iter_content(chunk_size=8192):
-                            if chunk:
-                                fl.write(chunk)
-
-                    local_file = None
-
-                    content_dsp = rsp.headers.get('content-disposition')
-                    if content_dsp:
-                        local_file = re.findall('filename="(.+)"', content_dsp)
-
-                    if local_file:
-                        local_file = local_file[0]
-
-                    if not local_file:
-                        parsed_url = urlparse.urlparse(image_url)
-                        local_file = os.path.basename(parsed_url.path)
-
-                    if not local_file:
-                        local_file = 'image.iso'
-
-                    temp_dir = tempfile.mkdtemp(
-                        dir=os.path.dirname(tmp_file.name))
-
-                    local_file_path = os.path.join(temp_dir, local_file)
-
-                os.rename(tmp_file.name, local_file_path)
-
-        except Exception as ex:
-            msg = 'Failed fetching image from URL %s: %s' % (image_url, ex)
-            self._logger.exception(msg)
-            raise error.FishyError(msg)
+        while retries < MAX_CREATE_LOCAL_IMAGE_RETRIES:
+            try:
+                local_file_path = self._fetch_and_create_local_image(image_url)
+                break  # Exit loop on success
+            except (error.FishyError, OSError) as ex:
+                retries += 1
+                if retries < MAX_CREATE_LOCAL_IMAGE_RETRIES:
+                    msg = (
+                        "Attempt %d failed fetching image from URL %s: %s. Retrying after %d seconds..."
+                        % (retries, image_url, ex, backoff)
+                    )
+                    self._logger.warning(msg)
+                    time.sleep(backoff)
+                    backoff *= BACKOFF_MULTIPLIER
+                else:
+                    msg = (
+                        "Max retries reached. Failed fetching image from URL %s: %s"
+                        % (image_url, ex)
+                    )
+                    self._logger.exception(msg)
+                    raise error.FishyError(msg)
 
         self._logger.debug(
-            'Fetched image %(file)s for %(identity)s' % {
-                'identity': identity, 'file': local_file})
+            "Fetched image %(file)s for %(identity)s"
+            % {"identity": identity, "file": os.path.basename(local_file_path)}
+        )
 
-        device_info['Image'] = image_url
-        device_info['Inserted'] = inserted
-        device_info['WriteProtected'] = write_protected
-        device_info['_local_file_path'] = local_file_path
+        device_info["Image"] = image_url
+        device_info["Inserted"] = inserted
+        device_info["WriteProtected"] = write_protected
+        device_info["_local_file_path"] = local_file_path
 
         self._devices.update({(identity, device): device_info})
 
+        return local_file_path
+
+    def _fetch_and_create_local_image(self, image_url):
+        """Fetch image from URL and create a local image file.
+
+        :param image_url: URL to ISO image
+        :returns: Path to the local file
+        :raises: `FishyError` if image can't be manipulated
+        """
+        with tempfile.NamedTemporaryFile(mode="w+b", delete=False) as tmp_file:
+            with requests.get(
+                image_url,
+                stream=True,
+                verify=False,
+                timeout=60,
+            ) as rsp:
+                if rsp.status_code >= 400:
+                    self._logger.error(
+                        "Failed fetching image from URL %s: " "got HTTP error %s:\n%s",
+                        image_url,
+                        rsp.status_code,
+                        rsp.text,
+                    )
+                    target_code = 502 if rsp.status_code >= 500 else 400
+                    raise error.FishyError(
+                        "Cannot download virtual media: got error %s "
+                        "from the server" % rsp.status_code,
+                        code=target_code,
+                    )
+
+                with open(tmp_file.name, "wb") as fl:
+                    for chunk in rsp.iter_content(chunk_size=8192):
+                        if chunk:
+                            fl.write(chunk)
+
+                local_file = None
+                content_dsp = rsp.headers.get("content-disposition")
+                if content_dsp:
+                    local_file = re.findall('filename="(.+)"', content_dsp)
+
+                if local_file:
+                    local_file = local_file[0]
+
+                if not local_file:
+                    parsed_url = urlparse.urlparse(image_url)
+                    local_file = os.path.basename(parsed_url.path)
+
+                if not local_file:
+                    local_file = "image.iso"
+
+                temp_dir = tempfile.mkdtemp(dir=os.path.dirname(tmp_file.name))
+                local_file_path = os.path.join(temp_dir, local_file)
+
+            os.rename(tmp_file.name, local_file_path)
         return local_file_path
 
     def eject_image(self, identity, device):
